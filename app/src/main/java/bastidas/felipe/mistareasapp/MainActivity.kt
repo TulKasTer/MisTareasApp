cc7471890ca7ff1ad59c4e80ec16261657566172
SOPORTE <soporte@CSNLABDM11.local>
2026-05-12 15:22:43 -0700

    Initial commit

---
+++ app/src/main/java/bastidas/felipe/mistareasapp/MainActivity.kt
@@ -0,0 +1,65 @@
+package bastidas.felipe.mistareasapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import bastidas.felipe.mistareasapp.ui.theme.MisTareasAppTheme
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        lifecycleScope.launch{
+            val db = AppDatabase.getInstance(
+                this@MainActivity
+            )
+
+            db.taskDao().insert(
+                TaskEntity(titulo = "Prueba")
+            )
+
+            val tareas = db.taskDao().getAllTasks().first()
+
+            Log.d("Room-Prueba", tareas.size.toString())
+        }
+        setContent {
+            MisTareasAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MisTareasAppTheme {
+        Greeting("Android")
+    }
+}
