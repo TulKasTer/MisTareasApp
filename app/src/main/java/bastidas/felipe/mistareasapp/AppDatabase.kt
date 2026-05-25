086c09397fcafe57861e0419968b4d23db97f3a0
TulKasTer <23609872+TulKasTer@users.noreply.github.com>
2026-05-25 00:44:01 -0700

    App v1.0

--- app/src/main/java/bastidas/felipe/mistareasapp/AppDatabase.kt
+++ app/src/main/java/bastidas/felipe/mistareasapp/AppDatabase.kt
@@ -1,9 +1,15 @@
 package bastidas.felipe.mistareasapp
 
 import android.content.Context
+import android.view.KeyEvent
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.SQLiteConnection
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlin.concurrent.Volatile
 
 @Database(
@@ -19,6 +25,73 @@ abstract class AppDatabase: RoomDatabase(){
         @Volatile
         private var INSTANCE: AppDatabase? = null
 
+        private val TAREAS_INICIALES = listOf(
+            TaskEntity(
+                titulo = "Configurar repositorio en GitHub",
+                completado = true
+            ),
+            TaskEntity(
+                titulo = "Implementar base de datos con Room",
+                completado = true
+            ),
+            TaskEntity(
+                titulo = "Construir UI con Jetpack Compose",
+                completado = true
+            ),
+            TaskEntity(
+                titulo = "Crear proyecto de Android",
+                completado = true
+            ),
+            TaskEntity(
+                titulo = "Crear Firebase",
+                completado = true
+            ),
+            TaskEntity(
+                titulo = "Configurar el Firebase en el Proyecto de Android",
+                completado = true
+            ),
+            TaskEntity(
+                titulo = "Hacer logica de Login",
+                completado = true
+            ),
+            TaskEntity(
+                titulo = "Hacer logica de SignUp",
+                completado = true
+            ),
+            TaskEntity(
+                titulo = "Manejar errores de firebase en las capas de datos",
+                completado = true
+            ),
+            TaskEntity(
+                titulo = "Manejar errores de firebase en las capas de dominio",
+                completado = true
+            ),
+            TaskEntity(
+                titulo = "Revisar PRs",
+                completado = false
+            ),
+            TaskEntity(
+                titulo = "Hacer logica de Restablecer contraseña",
+                completado = false
+            ),
+            TaskEntity(
+                titulo = "Hacer pantalla de Restablecer contraseña",
+                completado = false
+            ),
+            TaskEntity(
+                titulo = "Estudiar Clean Architecture de Android Developers",
+                completado = true
+            ),
+            TaskEntity(
+                titulo = "Estudiar MVVM de Android Developers",
+                completado = true
+            ),
+            TaskEntity(
+                titulo = "Estudiar convenciones de Android Developers",
+                completado = true
+            ),
+        )
+
         fun getInstance(
             context: Context
         ): AppDatabase {
@@ -29,8 +102,19 @@ abstract class AppDatabase: RoomDatabase(){
                     context.applicationContext,
                     AppDatabase::class.java,
                     "tasks_db"
-                ).build()
-                INSTANCE = instance
+                )
+                    .addCallback(object: RoomDatabase.Callback() {
+                    override fun onCreate(db: SupportSQLiteDatabase) {
+                        super.onCreate(db)
+                        CoroutineScope(Dispatchers.IO).launch {
+                            val dao = getInstance(context).taskDao()
+                            TAREAS_INICIALES.forEach { tarea ->
+                                dao.insert(tarea)
+                            }
+                        }
+                    }
+                }).build()
+
                 instance
             }
         }
